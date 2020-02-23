8eba1dd0d1ad7c1d5c50ad57ba98f6e25df51a05
yori <47884591+yoritin@users.noreply.github.com>
2020-02-23 11:10:46 +0900

    ボタンを押すとメッセージが変わる

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,37 @@
 import React from "react";
 import Sample from './Sample'
 
-class App extends React.Component {
+interface Props {}
+
+interface State {
+  message: string;
+}
+
+class App extends React.Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+
+    this.state = {
+      message: "最初のメッセージ"
+    };
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState({
+      message: "ボタンが押されました"
+    });
+  }
+
   render() {
     return (
       <React.Fragment>
         <p>Hello world</p>
         <Sample num={"5"} />
+
+      <p>{this.state.message}</p>
+      <button onClick={this.handleClick}>ボタン</button>
       </React.Fragment>
     );
   }
